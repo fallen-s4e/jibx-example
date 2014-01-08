3d232609c1e87395cef4ee3f6779a818af7e8458
magzhan.karasayev <fallen.s4e@gmail.com>
2014-01-08 17:41:34 +0600

    New: complete example finished

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,9 +1,12 @@
 
+import example.Address;
 import example.Customer;
 import org.jibx.runtime.*;
 
+import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.InputStream;
+import java.io.OutputStream;
 
 /**
  * Created with IntelliJ IDEA.
@@ -13,32 +16,36 @@ import java.io.InputStream;
  */
 public class Main {
     public static void main(String[] args) throws Exception {
-//        Customer message = read();
+        write(getDummyCustomer(), "customer.xml");
 
-        /*
-        model.setGroupId(groupId);
-        model.setArtifactId(artifactId);
-        */
-
-        write(new Customer(), "customer.xml");
+        write(read("customer.xml"), System.out);
     }
 
-    private static void write(Customer message, String fileName) throws Exception {
+    private static void write(Customer message, OutputStream os) throws Exception {
         IBindingFactory jc = BindingDirectory.getFactory(Customer.class);
         IMarshallingContext marshaller = jc.createMarshallingContext();
         marshaller.setIndent(4);
-        marshaller.marshalDocument(message, "utf-8", null, new FileOutputStream(fileName));
+        marshaller.marshalDocument(message, "utf-8", null, os);
         marshaller.getXmlWriter().flush();
-//
-//        IMarshallingContext mctx = jc.createMarshallingContext();
-//        mctx.setOutput(new FileOutputStream(fileName), null);
-//        ((IMarshallable)obj).marshal(mctx);
-//        mctx.getXmlWriter().flush();
     }
 
-    private static Customer read() throws JiBXException {
-        InputStream inStream = Thread.currentThread().getContextClassLoader().getResourceAsStream("xml/customer.xml");
+    private static void write(Customer message, String fileName) throws Exception {
+        write(message, new FileOutputStream(fileName));
+    }
+
+    private static Customer read(String fileName) throws Exception {
+        InputStream inStream = new FileInputStream(fileName);
         IBindingFactory jc = BindingDirectory.getFactory(Customer.class);
         return (Customer)jc.createUnmarshallingContext().unmarshalDocument(inStream, null);
+    }
+
+    public static Customer getDummyCustomer() {
+        Customer c = new Customer();
+        c.setAddress(new Address());
+        c.getAddress().setCity("1");
+        c.getAddress().setState("2");
+        c.getAddress().setStreet("3");
+        c.getAddress().setZip(4);
+        return c;
     }
 }
